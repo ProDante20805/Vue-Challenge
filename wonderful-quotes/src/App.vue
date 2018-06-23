546631f949f8cac0ac2e112e2849dc7fb04b375d
ProDante20805 <pro.guru0114@gmail.com>
2018-06-23 16:35:41 +0200

    Add wonderful-quotes project

---
+++ wonderful-quotes/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="container">
+    <app-quote-grid :for="quote in quotes" :quot="quotes">{{ quote }}</app-quote-grid>
+  </main>
+</template>
+
+<script>
+import QuoteGrid from "./components/quotes/QuotesGrid"
+
+export default {
+  quotes = ["Hello i'm a quote"],
+  components: {
+    "app-quote-grid": QuoteGrid
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ wonderful-quotes/src/components/quotes/QuotesGrid.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row">
+        <app-quote></app-quote>
+    </div>
+</template>
+
+<script>
+import Quote from "./Quote"
+
+export default {
+    props: {
+        quot: {
+            type: String
+        }
+    },
+    components: {
+        "app-quote": Quote
+    }
+}
+</script>
+
+<style>
+
+</style>
