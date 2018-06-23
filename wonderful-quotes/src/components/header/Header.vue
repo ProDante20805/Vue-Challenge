53bae7fec8bc76e7fa9c4a92c53fd7b186c43eee
ProDante20805 <pro.guru0114@gmail.com>
2018-06-23 23:58:06 +0200

    Project done

---
+++ wonderful-quotes/src/components/header/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row mt-2 mb-2">
+        <div class="col-12">
+            <h1 class="text-center">Quotes Added</h1>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" :style="{width: (quoteCount / quoteMax)  * 100 + '%' }" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{{ quoteCount }} / {{ quoteMax }}</div>
+                </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        quoteCount: {
+            type: Number
+        },
+        quoteMax: {
+            type: Number
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ wonderful-quotes/src/components/quotes/NewQuote.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row mt-2 mb-2">
+        <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 col-lg-12 col-lg-offset-6 mx-auto form-group">
+            <label for="areaText">Quote:</label>
+            <textarea name="" id="areaTex" rows="3" class="form-control" v-model="quoteLink"></textarea>
+        </div>
+        <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 form-group  mt-2">
+            <button class="btn btn-primary" @click.prevent="addNew">Add quote</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            quoteLink: ""
+        }
+    },
+    methods: {
+        addNew() {
+            this.$emit("quoteAdded", this.quoteLink);
+            this.quoteLink = ""
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
